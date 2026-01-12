77641a44a5ebfd75fac52bc0824566248726a078
adenercan <adenercan23@gmail.com>
2026-01-12 05:18:37 +0300

    display pomodoro time from state

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,16 @@
-import React from "react"
+import React, { useState } from "react"
 import { ModeSelector } from "./components/modeSelector"
 import { Controls } from "./components/controls"
 import { TimerDisplay } from "./components/timerDisplay"
 
 function App() {
- 
+  const [time, setTime] = useState(25 * 60);
 
   return (
     <>
     <div className="flex flex-col justify-center">
       <ModeSelector />
-      <TimerDisplay/>
+      <TimerDisplay time={time}/>
       <Controls/>
     </div>
     </>
--- src/components/timerDisplay.jsx
+++ src/components/timerDisplay.jsx
@@ -1,9 +1,13 @@
 import React from "react";
 
-export const TimerDisplay = () => {
+export const TimerDisplay = ({time}) => {
+
+    const minutes = Math.floor(time / 60);
+    const seconds = time % 60;
+
     return (
         <div className="text-6xl text-center">
-            25:00
+            {minutes}:{seconds.toString().padStart(2,"0")}
         </div>
     )
 }
